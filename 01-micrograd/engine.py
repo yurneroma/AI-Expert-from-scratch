3ecb1579263f8b7cc55cfa436fd18d1016ead36e
yurneroma <mabinzq007@126.com>
2026-07-05 20:51:00 +0800

    feature:实现反向传播，实现pow,sub,truediv,neg 等方法

--- 01-micrograd/engine.py
+++ 01-micrograd/engine.py
@@ -9,13 +9,73 @@ class Value:
 
   def __add__(self, other:Value):
     out = Value(self.data + other.data, (self, other), '+')
+    def _backward():
+      self.grad += 1.0*out.grad
+      other.grad  += 1.0*out.grad
+
+    out._backward = _backward
     return out
   
   def __mul__(self, other:Value):
     out = Value(self.data * other.data, (self, other), '*')
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+    out._backward = _backward
     return out
+
+  def __pow__(self, power):
+    assert isinstance(power, (int, float)), "only supports int/float powers"
+    out = Value(self.data**power, (self, ), f'**{power}')
+    def _backward():
+      self.grad += power * self.data ** (power - 1) * out.grad
+    out._backward = _backward
+    return out
+
+  def __neg__(self):
+    out = Value(-self.data, (self, ), '-')
+    def _backward():
+      self.grad += -1.0 * out.grad
+    out._backward = _backward
+    return out
+
+  def __sub__(self, other:Value):
+    return self + (-other)
+
+  def __truediv__(self, other:Value):
+    return self * other**-1
+
+  def __radd__(self, other):
+    pass
+
 
   def __repr__(self):
     return f"Value(data={self.data}, children={list(self._prev)}, op={self._op!r})"
 
+  def backward(self): 
+    self.grad = 1.0
+    topo = []
+    visited = set()
+
+    def build_topo(v):
+      """
+        Builds a topological ordering of the nodes in the graph ending at v.
+      """
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    
+    build_topo(self)
+
+    # backpropagate the gradients through the graph
+    for node in reversed(topo):
+      node._backward()
+
+
+
+
+
+  
   
